e2f6f0b7f77740dd717f529522febc221c879425
bhanu aluka <alukabhanuprakash@gmail.com>
2025-02-05 13:45:53 +0530

    inserted data in DB

--- src/app.js
+++ src/app.js
@@ -3,13 +3,11 @@ const { connectDB } = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Sai master",
-    lastName: "sai master",
-    emailId: "saimaster@gmail.com",
-    age: 0,
-  });
+  // created a new instance of the user model
+  const user = new User(req.body);
 
   try {
     await user.save();
