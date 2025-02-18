719c73a5350d705b34b06b4656d6e23e725106fc
bhanu aluka <alukabhanuprakash@gmail.com>
2025-02-18 17:37:14 +0530

    added cors policies

--- src/app.js
+++ src/app.js
@@ -14,12 +14,7 @@ const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/requests");
 const userRouter = require("./routes/user");
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
